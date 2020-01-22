14b6854f382145b806882ab569878acb39e46a32
David Van Fleet <dvfleet413@gmail.com>
2020-01-22 21:16:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,9 @@ def badge_maker(name)
   badge = "Hello, my name is #{name}."
 end
 
-def batch_badge_creator
+def batch_badge_creator(names)
+  badges = []
+  names.each {|name| badges << "Hello, my name is #{name}."
 end
 
 def assign_rooms
